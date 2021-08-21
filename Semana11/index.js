e6ec2289dacf10800b73e92237fb25aa9ea8411b
“Alexval03195” <alexval03195@gmail.com>
2021-08-21 17:27:16 -0500

    “semanas_11”

---
+++ Semana11/index.js
@@ -0,0 +1,23 @@
+
+
+const express= requiere('express');
+const webpack=requiere('webpack');
+const webpack_middleware=require('webpack-middleware');
+const webpackConfig=require('../webpack.config');
+
+var app= express();
+app.set('port',(process.env.PORT || 3000));
+app.use('/static',express.static(__dirname +'/dist'));
+app.use(webpack_middleware(webpackConfig));
+
+
+
+app.get('/',function(req,res,next){
+    res.send('Hola,3ct');
+});
+
+    app.listen(app.get('port'),()=>{
+        console.log('Servidor listo');
+   
+
+});
